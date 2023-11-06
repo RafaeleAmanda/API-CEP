2b4462436e7fa7ad13cbe53bf5d5e058f4bef8a0
RafaeleAmanda <amandagr1111p@gmail.com>
2023-11-05 21:52:50 -0300

    first commit

---
+++ serve.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    fs.readFile('enderecoCep.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Erro interno do servidor');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Página não encontrada');
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor em execução em http://localhost:${PORT}`);
+});
